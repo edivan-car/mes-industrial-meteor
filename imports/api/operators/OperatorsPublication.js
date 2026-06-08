d73997d3f0db76d79b386d73c7c1a6591370029f
edivan-car <edivam.c@gmail.com>
2026-06-08 14:51:38 -0300

    "feat: implementa LoginPage com busca e seleção de operador por matrícula"

--- imports/api/operators/OperatorsPublication.js
+++ imports/api/operators/OperatorsPublication.js
@@ -1,6 +1,5 @@
 import { Meteor } from "meteor/meteor";
 import { OperatorsCollection } from "./OperatorsCollection";
-import {data} from "autoprefixer";
 
 Meteor.publish("operators", function (){
     return OperatorsCollection.find();
